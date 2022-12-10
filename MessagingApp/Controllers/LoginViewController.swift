941f81e9c32058f7ab2bfee369715d72846e1959
Carrington <Manyuchi>
2022-12-10 19:55:53 +0200

    Added registration flow and login flow to our app

--- MessagingApp/Controllers/LoginViewController.swift
+++ MessagingApp/Controllers/LoginViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 import Foundation
-
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password) {  authResult, error in
+             
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
+        
     }
 }
--- MessagingApp/Controllers/RegisterViewController.swift
+++ MessagingApp/Controllers/RegisterViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 import Foundation
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+              // ...
+                
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    //Navigate to the ChatViewController
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        
+        }
+    }
+    
 }
