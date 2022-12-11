8671de86162e47712440c7b95d4c368a5d95b840
Carrington <Manyuchi>
2022-12-11 10:48:52 +0200

    Used Firebase firestorm to save data, to retrieve data, to listen for updates on our data, to sort and query our data

--- MessagingApp/Controllers/ChatViewController.swift
+++ MessagingApp/Controllers/ChatViewController.swift
@@ -34,8 +34,12 @@ class ChatViewController: UIViewController {
     
     func loadMessages() {
         
-        db.collection(K.FStore.collectionName).addSnapshotListener { querySnapshot, error in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
+                
             self.messages = []
+                
             if let e = error {
                 print("There was an issue in retrieving data from Firestore. \(e)")
             } else {
@@ -61,7 +65,8 @@ class ChatViewController: UIViewController {
         if let messageBody = messageTextfield.text,  let messageSender = Auth.auth().currentUser?.email{
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField: messageSender,
-                K.FStore.bodyField: messageBody
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
             ]) { (error) in
                 if let e = error {
                     print("There was an issue saving data tofirestore, \(e)")
